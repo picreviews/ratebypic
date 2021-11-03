a5040fe9bbd16e0549fd76f2457f7b32b0cfe18d
Seb Sebahattin Kumpinar <sebahattinkumpinar@gmail.com>
2021-11-03 14:13:51 +0300

    initial commit

---
+++ src/app/models/ratepic.model.ts
@@ -0,0 +1,6 @@
+export default class RatePic {
+  id?: string;
+  title?: string;
+  url?: string;
+  user?: any;
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCIM79p5BYaidpPLV-51pUYn8iRGIQpcWw",
+    authDomain: "seb-revenue-map.firebaseapp.com",
+    projectId: "seb-revenue-map",
+    storageBucket: "seb-revenue-map.appspot.com",
+    messagingSenderId: "572948188720",
+    appId: "1:572948188720:web:4294650e92ec1589b8a7f5"
+  }
+};
